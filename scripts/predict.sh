a9319d7498659e39abd7a6ae643bc25a39556688
ada-f <adafang.af@gmail.com>
2025-05-06 22:23:34 -0400

    adding baselines

--- scripts/predict.sh
+++ scripts/predict.sh
@@ -5,22 +5,37 @@
 #SBATCH -c 10 # number of cores
 #SBATCH --mem 500g # memory pool for all cores
 #SBATCH --gres=gpu:1 # gpu
-#SBATCH -t 3-00:00 # time (D-HH:MM)
+#SBATCH -t 0-1:00 # time (D-HH:MM)
 #SBATCH -o slurm/slurm.%N.%j.out # STDOUT
 #SBATCH -e slurm/slurm.%N.%j.err # STDERR
 
-# load your bash config and (re)activate the conda env
-source ~/.bashrc
-conda deactivate || true
-conda activate esm_env
 
 # change if needed
-project_dir="/n/holylfs06/LABS/mzitnik_lab/Users/${USER}/foldingdiff"
+project_dir="/n/holylfs06/LABS/mzitnik_lab/Lab/afang/PT-BPE"
 
 cd $project_dir
 
+module load cuda/12.4.1-fasrc01 cudnn/9.5.1.17_cuda12-fasrc01
+
 # train
-# python bin/predict.py --cuda cuda:0 --pkl-file ${project_dir}/ckpts/1744875790.3072364/bpe_iter=6000.pkl --pkl-data-file ${project_dir}/homo_datasets_with_test.pkl --auto
+PYTHONPATH=/n/holylfs06/LABS/mzitnik_lab/Lab/afang/PT-BPE \
+  /n/netscratch/mzitnik_lab/Lab/afang/envs/stbenv/bin/python -m bin.predict \
+  --cuda cuda:0 \
+  --pkl-file /n/holylfs06/LABS/mzitnik_lab/Users/msun415/foldingdiff/ckpts/1744875790.3072364/bpe_iter=6000.pkl \
+  --task BindInt \
+  --level residue \
+  --auto --debug
+
+  # --pkl-data-file ${project_dir}/data/enzyme_commission/EC2_seq30.pkl \
+  # ${project_dir}/ckpts/1745337217.4614503/bpe_iter=510.pkl \
 
 # test
-python bin/predict.py --cuda cuda:0 --pkl-file ${project_dir}/ckpts/1744875790.3072364/bpe_iter=6000.pkl --pkl-data-file ${project_dir}/homo_datasets_with_test.pkl --test --save-dir ${project_dir}/ckpts/1744932629.834232
+# python bin/predict.py --cuda cuda:0 --pkl-file ${project_dir}/ckpts/1745337217.4614503/bpe_iter=510.pkl --test --save-dir ${project_dir}/ckpts/1745337217.4614503
+
+# project_dir="/n/holylfs06/LABS/mzitnik_lab/Users/msun415/foldingdiff/"
+# PYTHONPATH=/n/holylfs06/LABS/mzitnik_lab/Lab/afang/PT-BPE \
+#   /n/netscratch/mzitnik_lab/Lab/afang/envs/esmenv/bin/python -m bin.predict \
+#   --cuda cuda:0 \
+#   --pkl-file ${project_dir}/ckpts/1744875790.3072364/bpe_iter=6000.pkl \
+#   --pkl-data-file ${project_dir}/homo_datasets_with_test.pkl \
+#   --auto
