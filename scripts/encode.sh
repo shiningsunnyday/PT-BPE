f81c12a69593a0812496ca63c68772ec1042280a
ada-f <adafang.af@gmail.com>
2025-05-07 16:34:21 -0400

    updated predictor code for residue level binary classification tasks

--- scripts/encode.sh
+++ scripts/encode.sh
@@ -3,9 +3,9 @@
 #SBATCH -p test # partition (queue)
 #SBATCH -c 10 # number of cores
 #SBATCH --mem 50g # memory pool for all cores
-#SBATCH -t 0-6:00 # time
-#SBATCH -o /n/netscratch/mzitnik_lab/Lab/afang/job_status/PTBPE_encode.%N.%j.out # STDOUT
-#SBATCH -e /n/netscratch/mzitnik_lab/Lab/afang/job_status/PTBPE_encode.%N.%j.err # STDERR
+#SBATCH -t 0-12:00 # time
+#SBATCH -o /n/netscratch/mzitnik_lab/Lab/afang/job_status/PTBPE_encode.%j.out # STDOUT
+#SBATCH -e /n/netscratch/mzitnik_lab/Lab/afang/job_status/PTBPE_encode.%j.err # STDERR
 
 # load your bash config and (re)activate the conda env
 # source ~/.bashrc
@@ -14,6 +14,9 @@
 
 # change if needed
 cd "/n/holylfs06/LABS/mzitnik_lab/Lab/afang/PT-BPE"
+
+# module load ncf/1.0.0-fasrc01; module load miniconda3/4.12.0-ncf; module load python/3.10.12-fasrc01
+# conda env create --prefix /n/netscratch/mzitnik_lab/Lab/afang/envs/ptbpe --file environment.yml
 
 toy=$1
 pad="512"
@@ -25,7 +28,7 @@ res_init="residue"
 
 # base command
 PYTHONPATH=/n/holylfs06/LABS/mzitnik_lab/Lab/afang/PT-BPE \
-  /n/netscratch/mzitnik_lab/Lab/afang/envs/stbenv/bin/python -m bin.encode \
+  /n/netscratch/mzitnik_lab/Lab/afang/envs/ptbpe/bin/python -m bin.encode \
   --auto \
   --bin-strategy histogram \
   --bins 1-20 \
